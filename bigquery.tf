2f12f8b8d42734dfce14be515a873800cd2d8c07
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-01-15 14:13:26 -0500

    adding trigger
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- bigquery.tf
+++ bigquery.tf
@@ -1,26 +1,30 @@
 resource "google_bigquery_dataset" "ecomm" {
-  dataset_id                  = "${var.project_name}_dataset"
-  description                 = "Dataset for ${var.project_name} project"
-  location                    = "US"
-  project                     = var.project_id
+  dataset_id  = "${var.project_name}_dataset"
+  description = "Dataset for ${var.project_name} project"
+  location    = "US"
+  project     = var.project_id
 
-  depends_on = [ google_project_service.project_service ]
+  depends_on = [google_project_service.project_service]
 }
 
 module "schemas" {
-    source = "./tables"
-    project_id = var.project_id
-    dataset_id = google_bigquery_dataset.ecomm.dataset_id
+  source     = "./tables"
+  project_id = var.project_id
+  dataset_id = google_bigquery_dataset.ecomm.dataset_id
 
-    depends_on = [ google_bigquery_dataset.ecomm ]
+  depends_on = [google_bigquery_dataset.ecomm]
 }
 
-resource "null_resource" "health_check" {
+resource "null_resource" "build_schema" {
 
- provisioner "local-exec" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
 
     command = "${path.module}/scripts/build_graphql_schema.sh ${var.common_project_id}"
   }
 
-  depends_on = [ module.schemas ]
+  depends_on = [module.schemas]
 }
