20e2ddc7db2b1c35bad070ff09e7e558b994fe96
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-01-15 15:20:05 -0500

    cleanup
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- scripts/build_graphql_schema.sh
+++ scripts/build_graphql_schema.sh
@@ -70,10 +70,7 @@ function main() {
   async function query() {
     // Queries the U.S. given names dataset for the state of Texas.
 
-    const query = `SELECT name
-      FROM \`bigquery-public-data.usa_names.usa_1910_2013\`
-      WHERE state = 'TX'
-      LIMIT 100`;
+    const query = "SELECT name FROM \`bigquery-public-data.usa_names.usa_1910_2013\` WHERE state = 'TX' LIMIT 100";
 
     // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
     const options = {
@@ -84,7 +81,6 @@ function main() {
 
     // Run the query as a job
     const [job] = await bigquery.createQueryJob(options);
-    console.log(`Job ${job.id} started.`);
 
     // Wait for the query to finish
     const [rows] = await job.getQueryResults();
--- scripts/schema_builder.js
+++ scripts/schema_builder.js
@@ -1,16 +1,3 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 import { BigQuery } from "@google-cloud/bigquery";
 
 function main() {
@@ -23,10 +10,7 @@ function main() {
   async function query() {
     // Queries the U.S. given names dataset for the state of Texas.
 
-    const query = `SELECT name
-      FROM \`bigquery-public-data.usa_names.usa_1910_2013\`
-      WHERE state = 'TX'
-      LIMIT 100`;
+    const query = "SELECT name FROM \`bigquery-public-data.usa_names.usa_1910_2013\` WHERE state = 'TX' LIMIT 100";
 
     // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
     const options = {
@@ -37,7 +21,6 @@ function main() {
 
     // Run the query as a job
     const [job] = await bigquery.createQueryJob(options);
-    console.log(`Job ${job.id} started.`);
 
     // Wait for the query to finish
     const [rows] = await job.getQueryResults();
