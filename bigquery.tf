17a325b9fb53a27698210235869a0b8fc1e18881
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-01-15 13:26:15 -0500

    adding script call
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- bigquery.tf
+++ bigquery.tf
@@ -15,57 +15,8 @@ module "schemas" {
     depends_on = [ google_bigquery_dataset.ecomm ]
 }
 
-# resource "google_bigquery_table" "admin_user" {
-#   dataset_id = google_bigquery_dataset.ecomm.dataset_id
-#   table_id   = "admin_user"
-#   deletion_protection = false
-#   project                     = var.project_id
-#   schema = <<EOF
-# [
-#   {
-#     "mode": "REQUIRED",
-#     "name": "admin_user_id",
-#     "type": "INTEGER"
-#   },
-#   {
-#     "defaultValueExpression": "GENERATE_UUID()",
-#     "mode": "REQUIRED",
-#     "name": "uuid",
-#     "type": "STRING"
-#   },
-#   {
-#     "defaultValueExpression": "true",
-#     "mode": "REQUIRED",
-#     "name": "status",
-#     "type": "BOOLEAN"
-#   },
-#   {
-#     "mode": "REQUIRED",
-#     "name": "email",
-#     "type": "STRING"
-#   },
-#   {
-#     "mode": "REQUIRED",
-#     "name": "password",
-#     "type": "STRING"
-#   },
-#   {
-#     "name": "full_name",
-#     "type": "STRING"
-#   },
-#   {
-#     "defaultValueExpression": "CURRENT_TIMESTAMP",
-#     "name": "created_at",
-#     "type": "TIMESTAMP"
-#   },
-#   {
-#     "defaultValueExpression": "CURRENT_TIMESTAMP",
-#     "name": "updated_at",
-#     "type": "TIMESTAMP"
-#   }
-# ]
-# EOF
+data "external" "build_graphql_schema" {
+  program = ["${path.module}/scripts/build_graphql_schema.sh", "${var.common_project_id}"]
 
-#   depends_on = [ google_bigquery_dataset.ecomm ]
-# }
-
+  depends_on = [ module.schemas ]
+}
---
+++ scripts/build_graphql_schema.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+sudo apt install nodejs npm
+
+curl https://sdk.cloud.google.com > install.sh
+bash install.sh --disable-prompts --install-dir=./$dirstring >/dev/null 
+PATH=$PATH:./$dirstring/google-cloud-sdk/bin
+printf '%s' "$GOOGLE_CREDENTIALS" > key.json
+gcloud auth activate-service-account --key-file=key.json
+
+SHA="test"
+
+cat <<EOF
+{
+  "sha": "$SHA"
+}
+EOF
