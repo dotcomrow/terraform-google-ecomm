5755a99c0c053aab1c12dd58ec562c07466b6a9e
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-01-15 15:58:29 -0500

    commit
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- scripts/build_graphql_schema.sh
+++ scripts/build_graphql_schema.sh
@@ -1,18 +1,3 @@
-#!/bin/sh
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
@@ -58,14 +43,8 @@ EOF
 npm install
 
 cat > schema_builder.js <<EOF
-{JS_CONTENTS}
+JS_CONTENTS
 EOF
-
-# curl https://sdk.cloud.google.com > install.sh
-# bash install.sh --disable-prompts --install-dir=./$dirstring >/dev/null 
-# PATH=$PATH:./$dirstring/google-cloud-sdk/bin
-# printf '%s' "$GOOGLE_CREDENTIALS" > key.json
-# gcloud auth activate-service-account --key-file=key.json
 
 printf '%s' "$GOOGLE_CREDENTIALS" > key.json
 node schema_builder.js
