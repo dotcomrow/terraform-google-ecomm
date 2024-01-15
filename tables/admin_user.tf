714afdcaf9b2c03246aa02eafb835ac064e7545e
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-01-15 13:34:49 -0500

    test
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- tables/admin_user.tf
+++ tables/admin_user.tf
@@ -45,6 +45,10 @@ resource "google_bigquery_table" "admin_user" {
     "defaultValueExpression": "CURRENT_TIMESTAMP",
     "name": "updated_at",
     "type": "TIMESTAMP"
+  },
+  {
+    "name":"nothing",
+    "type":"STRING"
   }
 ]
 EOF
