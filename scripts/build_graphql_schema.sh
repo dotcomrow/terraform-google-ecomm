edc64bedfe17b3b8088b197e9276bfb93df7f906
mephmanx <mephmanx@users.noreply.github.com>
2024-01-19 22:07:49 +0000

    Release 2024.1.56

--- scripts/build_graphql_schema.sh
+++ scripts/build_graphql_schema.sh
@@ -7,13 +7,199 @@ export NVM_DIR="$HOME/.nvm"
 nvm install node
 
 cat > package.json <<EOF
-PKG_JSON_CONTENTS
+{
+    "name": "schema-builder",
+    "version": "1.0.0",
+    "dependencies": {
+      "serialize-error": "^11.0.3"
+    },
+    "devDependencies": {
+      "webpack": "^5.89.0",
+      "webpack-cli": "^5.1.4",
+      "babel-loader": "^9.1.3",
+      "path-browserify": "^1.0.1",
+      "crypto-browserify": "3.12.0",
+      "stream-browserify":"^3.0.0",
+      "https-browserify":"^1.0.0",
+      "os-browserify":"^0.3.0",
+      "browserify-zlib":"^0.2.0",
+      "util":"^0.12.5",
+      "url":"^0.11.3",
+      "stream-http":"^3.2.0",
+      "assert":"^2.1.0",
+      "@google-cloud/bigquery":"^7.3.0",
+      "fs":"^0.0.1-security",
+      "querystring-es3":"^0.2.1",
+      "net-browserify":"^0.2.4",
+      "process":"^0.11.10",
+      "buffer":"^6.0.3",
+      "graphql":"^16.8.1",
+      "@graphql-tools/schema":"^9.0.0",
+      "node-cloudflare-r2":"0.1.5"
+    },
+    "private": true,
+    "type": "module",
+    "main": "index.js"
+}
 EOF
 
 npm install
 
 cat > schema_builder.js <<EOF
-JS_CONTENTS
+import { BigQuery } from "@google-cloud/bigquery";
+import { GraphQLSchema, 
+  GraphQLScalarType, 
+  GraphQLObjectType,
+  GraphQLString, 
+  GraphQLInt, 
+  GraphQLBoolean,
+  introspectionFromSchema } from "graphql";
+import fs from 'fs';
+import { serializeError } from "serialize-error";
+import { mergeSchemas } from '@graphql-tools/schema';
+import { R2 } from 'node-cloudflare-r2';
+
+function main() {
+  const options = {
+    keyFilename: "key.json",
+    projectId: "$1",
+    datasetId: "$2",
+    // bucket_name: "$3",
+  };
+  const bigquery = new BigQuery(options);
+  // const storage = new Storage({
+  //   projectId: options.projectId,
+  //   keyFilename: "storage_admin_key.json",
+  // });
+
+  const r2 = new R2({
+      accountId: "$4",
+      accessKeyId: "$5",
+      secretAccessKey: "$6",
+  });
+
+  const bucket = r2.bucket("$3");
+  const fileName = 'graphql_schema.json';
+
+  function getTableMetadata(table) {
+    async function getTM(table) {
+      const metadata = await table.getMetadata();
+      return metadata;
+    }
+    return getTM(table);
+  }
+
+  async function uploadFile() {
+    
+    // var generationMatchPrecondition = 0
+
+    // const local_options = {
+    //   destination: fileName,
+    //   // Optional:
+    //   // Set a generation-match precondition to avoid potential race conditions
+    //   // and data corruptions. The request to upload is aborted if the object's
+    //   // generation number does not match your precondition. For a destination
+    //   // object that does not yet exist, set the ifGenerationMatch precondition to 0
+    //   // If the destination object already exists in your bucket, set instead a
+    //   // generation-match precondition using its generation number.
+    //   preconditionOpts: {ifGenerationMatch: generationMatchPrecondition},
+    // };
+
+    // await storage.bucket(options.bucket_name).upload(fileName, local_options);
+
+    // Set your bucket's public URL
+    // bucket.provideBucketPublicUrl('https://pub-xxxxxxxxxxxxxxxxxxxxxxxxx.r2.dev');
+
+    // console.log(await bucket.exists());
+    // true
+
+    const upload = await bucket.uploadFile(fileName, fileName);
+  }
+
+  function parseType(field) {
+    switch (field.type) {
+      case "STRING":
+        return GraphQLString;
+      case "INTEGER":
+        return GraphQLInt;
+      case "BOOLEAN":
+        return GraphQLBoolean;
+      case "FLOAT":
+        return GraphQLFloat;
+      case "TIMESTAMP":
+        return GraphQLString;
+      case "DATE":
+        return GraphQLString;
+      case "TIME":
+        return GraphQLString;
+      case "DATETIME":
+        return GraphQLString;
+      case "GEOGRAPHY":
+        return GraphQLString;
+      case "NUMERIC":
+        return GraphQLInt;
+      default:
+        return GraphQLString;
+    }
+  }
+
+  async function fetchSchemas() {
+    const [tables] = await bigquery.dataset(options.datasetId).getTables();
+
+    var tableList = [];
+    var graphqlObjects = [];
+    for (var table of tables) {
+      var tableMetadata = await getTableMetadata(table);
+      tableList.push(tableMetadata);
+    }
+
+    for (var metadata of tableList) {
+      var fields = {};
+      metadata[0].schema.fields.forEach((field) => {
+        fields[field.name] = {
+          type: parseType(field.type),
+          description: field.description,
+        };
+      });
+
+      var graphqlObj = new GraphQLObjectType({
+        name: metadata[0].tableReference.tableId,
+        fields: fields,
+      });
+
+      var graphqlSchema = new GraphQLSchema({
+        query: graphqlObj
+      });
+
+      graphqlObjects.push(
+        graphqlSchema
+      );
+    }
+
+    return graphqlObjects;
+  }
+
+  async function query() {
+    var schemas = await fetchSchemas();
+    const storage = new Storage();
+    const mergedSchema = mergeSchemas({
+      schemas: schemas
+    })
+    const schema_json = introspectionFromSchema(mergedSchema);
+    let json = JSON.stringify(schema_json);
+    // console.log(json);
+    await fs.writeFile(fileName, json, (err) => err && console.error(err));
+    await uploadFile();
+  }
+
+  try {
+    query();
+  } catch (err) {
+    const responseError = serializeError(err);
+    console.error(responseError);
+  }
+}
+main(...process.argv.slice(2));
 EOF
 
 # ls -al
