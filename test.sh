ed6127af742b0d652a9068cd5055afc73678ed00
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-01-20 20:31:39 -0500

    adjust
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- test.sh
+++ test.sh
@@ -4,10 +4,10 @@ CONTENTS=""
 LIST=$(find . -name '*.graphql')
 for f in $LIST; do
   FILENAME=$(echo "$f" | rev | cut -d "/" -f1 | rev)
-  CONTENTS="$CONTENTS cat $FILENAME <<EOF " >> CONTENTS
+  CONTENTS="$CONTENTS""cat $FILENAME <<EOF" >> CONTENTS
   CONTENTS="$CONTENTS$(cat $f)" >> CONTENTS
-  CONTENTS="$CONTENTS EOF " >> CONTENTS
+  CONTENTS="$CONTENTS""EOF\n" >> CONTENTS
 done
 
-echo $CONTENTS > graphql_contents
+echo -e $CONTENTS > graphql_contents
 sed -e '/GRAPHQL_CONTENTS/{r graphql_contents' -e 'd;}' build_graphql_schema.sh > build_graphql_schema.sh.new
