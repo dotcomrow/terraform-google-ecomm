888f8e68897e617ab095f6fc94b24456f6b6c150
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-01-28 13:59:08 +0000

    Deploying to release from @ dotcomrow/terraform-google-ecomm@5966cc1c7c49496269ed9885a0cbdf1a38c0fa05 🚀

---
+++ tables/attribute_group.tf
@@ -0,0 +1,36 @@
+resource "google_bigquery_table" "attribute_group" {
+  dataset_id = var.dataset_id
+  table_id   = "attribute_group"
+  deletion_protection = false
+  project                     = var.project_id
+  schema = <<EOF
+[
+  {
+    "mode": "REQUIRED",
+    "name": "attribute_group_id",
+    "type": "INTEGER"
+  },
+  {
+    "defaultValueExpression": "GENERATE_UUID()",
+    "mode": "REQUIRED",
+    "name": "uuid",
+    "type": "STRING"
+  },
+  {
+    "mode": "REQUIRED",
+    "name": "group_name",
+    "type": "STRING"
+  },
+  {
+    "defaultValueExpression": "CURRENT_TIMESTAMP",
+    "name": "created_at",
+    "type": "TIMESTAMP"
+  },
+  {
+    "defaultValueExpression": "CURRENT_TIMESTAMP",
+    "name": "updated_at",
+    "type": "TIMESTAMP"
+  }
+]
+EOF
+}
